c81cc8a00070453cfa2317ac4752691b0f75ff54
shwatanap <watanabeshou1520@gmail.com>
2023-10-19 10:27:30 +0900

    応答メッセージのキーワードをはじく処理実装

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
+app.secret_key = 'prev_msg'
 
-line_bot_api = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)    # config.pyで設定したチャネルアクセストークン
-handler = WebhookHandler(config.LINE_CHANNEL_SECRET)    # config.pyで設定したチャネルシークレット
+# config.pyで設定したチャネルアクセストークン
+LINE_CHANNEL_ACCESS_TOKEN = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
+# config.pyで設定したチャネルシークレット
+LINE_CHANNEL_SECRET = WebhookHandler(config.LINE_CHANNEL_SECRET)
+# LINEBotの応答メッセージで使う単語
+REPLY_KEYWORDS = ["レシピ", "食材", "ジャンル"]
 
 
 @app.route("/callback", methods=['POST'])
@@ -28,7 +33,7 @@ def callback():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        LINE_CHANNEL_SECRET.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
@@ -36,14 +41,21 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@LINE_CHANNEL_SECRET.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # APIのヘルスチェック用
     if event.reply_token == "00000000000000000000000000000000":
         return
 
-    line_bot_api.reply_message(
+    user_input = event.message.text
+
+    # 応答メッセージで設定されている単語が含まれていたら、何もしない
+    if user_input in REPLY_KEYWORDS:
+        return
+
+    LINE_CHANNEL_ACCESS_TOKEN.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=user_input)
     )
 
 if __name__ == "__main__":
